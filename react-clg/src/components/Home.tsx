60531aff67720c8668e455dbdce9646c13531ea6
unknown <nraisane@asu.edu>
2023-03-05 14:02:32 -0800

    created results page, fully working MVP

--- react-clg/src/components/Home.tsx
+++ react-clg/src/components/Home.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 function Home() {
   const [jd, setJd] = useState("");
   const [prompt, setPrompt] = useState("");
   const [coverLetter, setCoverLetter] = useState("");
+  const navigate = useNavigate();
 
-  const doTheMagic = async () => {
+  // redirect to result page after cover letter is generated
+  useEffect(() => {
+    if (coverLetter !== "") {
+      navigate("/result", { state: { coverLetter } });
+    }
+  }, [coverLetter])
+
+  const doTheMagic = async (): Promise<void> => {
     try {
       const uri = "https://npqp27hv70.execute-api.us-east-1.amazonaws.com/sorcery";
       const response = await fetch(uri, {
@@ -16,7 +25,10 @@ function Home() {
           jobDescription: jd,
         })
       });
-      return setCoverLetter(String(response.body));
+      const { coverLetter } = await response.json();
+      console.log(coverLetter);
+      setCoverLetter(coverLetter);
+    
     } catch (err) {
       console.error(err);
     }
@@ -24,16 +36,17 @@ function Home() {
 
   return (
     <div className="Home">
-    <p>
+    <p className="titles">
         Paste Job Description
     </p>
     <textarea className="job-description" onChange={(e) => {setJd(e.target.value)}}/>
 
-    <p>
+    <p className="titles">
         Write a few sentences about your skills, experience, and what you're looking for
     </p>
     <textarea className="prompt" onChange={(e) => {setPrompt(e.target.value)}}/>
-    <button className="button accept-btn">
+    <br/>
+    <button className="button accept-btn" onClick={() => doTheMagic()}>
     Generate Cover Letter
     </button>
     </div>
--- react-clg/src/components/Result.tsx
+++ react-clg/src/components/Result.tsx
@@ -1,10 +1,30 @@
 import "../App.css";
+import { useLocation, useNavigate } from 'react-router-dom'; 
 
 function Result () {
+    const location = useLocation();
+    const navigate = useNavigate();
   
+  // if user did not follow process, redirect to home page
+  if (!location.state) {
+    return (
+      <div className="result-page">
+        <h1>Oops! Something went wrong!</h1>
+        <p className="titles">Try going back to the home page and try again.</p>
+        <button className="button accept-btn" onClick={() => navigate("/")}>
+          Take me back
+        </button>
+      </div>
+    );
+  }
   return (
-    <div className="Result">
-        this is the result page
+    <div className="result-page">
+      <h1>Your cover letter is ready!</h1>
+      <textarea className="result" defaultValue={location.state.coverLetter}/>
+      <br/>
+      <button className="button accept-btn" onClick={() => navigate("/")}>
+        Generate another!
+      </button>
     </div>
   );
 }
