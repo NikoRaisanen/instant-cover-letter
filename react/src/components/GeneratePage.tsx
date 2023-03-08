da00401a0f8d65dd4ab8cba9384bb6601673ede7
unknown <nraisane@asu.edu>
2023-03-07 21:54:54 -0800

    character counter for job description on frontend

--- react/src/components/GeneratePage.tsx
+++ react/src/components/GeneratePage.tsx
@@ -10,7 +10,10 @@ function GeneratePage() {
   const [coverLetter, setCoverLetter] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [jdLength, setJdLength] = useState(0);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
   const navigate = useNavigate();
+  const maxJdLength = 8000;
 
   // redirect to result page after cover letter is generated
   useEffect(() => {
@@ -20,9 +23,9 @@ function GeneratePage() {
   }, [coverLetter])
 
   const doTheMagic = async (): Promise<void> => {
-    if (prompt === "" || jd === "") {
-      return alert("Please enter both a job descripton and a summary of your skills");
-    }
+    // if (prompt === "" || jd === "") {
+    //   return alert("Please enter both a job descripton and a summary of your skills");
+    // }
     setLoading(true);
     try {
       const uri = "https://npqp27hv70.execute-api.us-east-1.amazonaws.com/sorcery";
@@ -42,6 +45,17 @@ function GeneratePage() {
     }
   }
 
+  const handleJdChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    setJd(e.target.value);
+    setJdLength(e.target.value.length);
+    if (e.target.value.length > maxJdLength) {
+      alert("Your job description is too long. Please shorten it to 8000 characters or less");
+      return setIsButtonDisabled(true);
+    }
+    setIsButtonDisabled(false);
+
+  }
+
   if (error) {
     return (
       <ErrorPage/>
@@ -53,7 +67,9 @@ function GeneratePage() {
     <p className="titles">
         Paste Job Description
     </p>
-    <textarea placeholder="Copy and paste a job description here" className="job-description" onChange={(e) => {setJd(e.target.value)}}/>
+    <label className="job-description-label">Characters remaining: {(maxJdLength - jdLength) < 0 ? 0 : (maxJdLength - jdLength)}</label>
+    <br></br>
+    <textarea placeholder="Copy and paste a job description here" className="job-description" onChange={(e) => {handleJdChange(e)}}/>
 
     <p className="titles">
         Write a few sentences about your skills, experience, and what you're looking for
@@ -62,8 +78,8 @@ function GeneratePage() {
     <br/>
     {
       loading ? <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div> :
-      <button className="button accept-btn" onClick={() => doTheMagic()}>
-      Generate Cover Letter
+      <button disabled={isButtonDisabled} className={isButtonDisabled ? "button cancel-btn": "button accept-btn"} onClick={() => doTheMagic()}>
+      {isButtonDisabled ? "Fix Inputs" : "Generate Cover Letter"}
       </button>
     }
     </div>
