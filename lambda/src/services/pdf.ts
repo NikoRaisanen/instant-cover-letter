a47c2384b5956c2d743ae51fd7e8cdd9df87159e
unknown <nraisane@asu.edu>
2023-03-12 14:01:55 -0700

    add multi-page support for pdf parsing

--- lambda/src/services/pdf.ts
+++ lambda/src/services/pdf.ts
@@ -1,17 +1,27 @@
 const pdfjs = require('pdfjs-dist/legacy/build/pdf.js')
 
 
-const getContent = async (src: any) => {
+const getContent = async (src: any): Promise<string> => {
+    const fileContents = [];
     const doc = await pdfjs.getDocument(src).promise;
-    const page = await doc.getPage(1);
-    return await page.getTextContent();
+    const numPages = doc.numPages;
+    console.log('numPages', numPages);
+
+    for (let currPage = 1; currPage <= numPages; currPage++) {
+        const page = await doc.getPage(currPage);
+        const content = await page.getTextContent();
+        const returned = concatStrings(content);
+        fileContents.push(...returned);
+    }
+    
+    return fileContents.join(' ');
 }
 
-const getItems = async (src: any) => {
-    const content = await getContent(src);
+const concatStrings = (content: any): string[] => {
+    console.log('content items', content.items);
     return content.items.map((item: { str: any; }) => item.str);
 }
 
-getItems("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf").then((res) => {
-    console.log('my result', res);
-});
+// getContent("https://instantcoverletter-resumes.s3.amazonaws.com/3pageResume.pdf").then((res) => {
+//     console.log('my result', res);
+// });
