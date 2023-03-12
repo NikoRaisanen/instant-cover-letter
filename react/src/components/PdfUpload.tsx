f1f2d95e3dba7d368a2023d31eb44ee73388baf5
unknown <nraisane@asu.edu>
2023-03-12 12:50:01 -0700

    frontend upload component with successful PUT request to s3

--- react/src/components/PdfUpload.tsx
+++ react/src/components/PdfUpload.tsx
@@ -1,37 +1,31 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
-const { Buffer } = require('buffer');
-const pdfjsLib = require("pdfjs-dist/build/pdf");
-const pdfjsWorker = require("pdfjs-dist/build/pdf.worker.entry");
+import axios from 'axios';
 
 const PdfUpload = (props: any) => {
-    const parsePdf = async (file: any) => {
-        pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-        console.log('type of file', typeof file);
-        const buffer = Buffer.from(file);
-        console.log('buffer', buffer);
-        // const pdfData = atob(file);
-        // console.log(pdfData);
-        // const fileData = fileReader.readAsArrayBuffer(buffer);
-        const pdf = await pdfjsLib.getDocument(buffer);
-        const numPages = pdf.numPages;
-        const pages = [];
-        for (let i = 1; i <= numPages; i++) {
-            const page = await pdf.getPage(i);
-            const text = await page.getTextContent();
-            pages.push(text.items.map((item: any) => item.str).join(' '));
-        }
-        console.log('pages', pages);
-        return pages;
-    }
+    const [selectedFile, setSelectedFile] = useState<any>(null);
+
+    const onFileChange = (e: any) => {
+        setSelectedFile(e.target.files[0]);
+    };
+
+    const onFileUpload = async () => {
+        const formData = new FormData();
+
+        formData.append(
+          "resume",
+          selectedFile,
+          selectedFile.name
+        );
+       
+        await axios.put(`https://instantcoverletter-resumes.s3.amazonaws.com/${selectedFile.name}`, formData);
+      };
 
     return (
         <div className="pdf-upload">
-            <h1>Upload a pdf</h1>
-            <p className="titles">Try going back to the home page and try again.</p>
-            <p style={{"fontSize": "14px", "paddingBottom": "25px"}}>{props.msg}</p>
-            <button onClick={() => parsePdf('./test.pdf')}>
-                Take me back
-            </button>
+            <input type="file" onChange={(e) => onFileChange(e)} />
+                <button onClick={() => onFileUpload()}>
+                  Upload!
+                </button>
         </div>
     );
 }
