b26cacde2742c09635b79b18975f283822a31f2c
unknown <nraisane@asu.edu>
2023-03-12 15:13:44 -0700

    stopping point

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,10 +1,14 @@
 import { generateCoverLetter } from "./services/openai";
+import { getContent } from "./services/pdf";
 
 exports.handler = async (event: { body: string; }, _: any) => {
     let response;
     try {
         const body = JSON.parse(event.body);
-        const { prompt, jobDescription } = body;
+        const { jobDescription } = body;
+        console.log('body', body);
+        const prompt = body.prompt || await getContent(body.resumeUrl);
+        console.info("prompt:", prompt)
         const coverLetter = await generateCoverLetter(jobDescription, prompt);
         if (!coverLetter) {
             throw new Error("ChatGPT could not create a cover letter from the provided information");
--- lambda/src/services/pdf.ts
+++ lambda/src/services/pdf.ts
@@ -1,20 +1,26 @@
 const pdfjs = require('pdfjs-dist/legacy/build/pdf.js')
 
 
-const getContent = async (src: any): Promise<string> => {
-    const fileContents = [];
-    const doc = await pdfjs.getDocument(src).promise;
-    const numPages = doc.numPages;
-    console.log('numPages', numPages);
+export const getContent = async (src: any): Promise<string> => {
+    console.log('inside getContent for src: ', src)
+    try {
+        const fileContents = [];
+        const doc = await pdfjs.getDocument(src).promise;
+        const numPages = doc.numPages;
+        console.log('numPages', numPages);
 
-    for (let currPage = 1; currPage <= numPages; currPage++) {
-        const page = await doc.getPage(currPage);
-        const content = await page.getTextContent();
-        const returned = concatStrings(content);
-        fileContents.push(...returned);
+        for (let currPage = 1; currPage <= numPages; currPage++) {
+            const page = await doc.getPage(currPage);
+            const content = await page.getTextContent();
+            const returned = concatStrings(content);
+            fileContents.push(...returned);
+        }
+
+        return fileContents.join(' ');
+    } catch (err) {
+        console.error('error parsing pdf for content', err);
+        throw new Error('Failed to parse pdf, try writing a prompt instead or ensure that the uploaded file is a valid pdf', err.message);
     }
-    
-    return fileContents.join(' ');
 }
 
 const concatStrings = (content: any): string[] => {
