5ea64bd7bd0eb856d5781b9e6fc0af55e0d95dda
NikoRaisanen <nikoraisanenbusiness@gmail.com>
2023-04-10 15:52:06 -0700

    created fileCheck method. TODO: disable button if file is invalid, find tooltip library to display errors

--- react/src/components/PdfUpload.tsx
+++ react/src/components/PdfUpload.tsx
@@ -2,22 +2,32 @@ import { useState } from 'react';
 import axios from 'axios';
 import '../App.css';
 import upload from '../images/upload.svg';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // TODO: disable button if filetype or size is invalid
 const PdfUpload = (props: any) => {
+    const [isValid, setIsValid] = useState(false);
+
     // only allow pdf files
-    const pdfCheck = (file: any) => {
-        if (file.type !== 'application/pdf') {
-            alert('Uploaded resume must be in pdf format');
+    const fileCheck = (file: any) => {
+        const isPdf = file.type === 'application/pdf';
+        if (!isPdf) {
+            alert('Please upload a resume in pdf format');
+            setIsValid(false);
+        }
+        else if (file.size > 1000000) {
+            alert('Please upload a resume that is less than 1MB');
+            setIsValid(false);
+        } else {
+            setIsValid(true);
         }
     };
 
     const onFileChange = (e: any) => {
         // TODO: check file type. Only allow pdfs
         console.log('file: ', e.target.files[0]);
-        pdfCheck(e.target.files[0]);
+        fileCheck(e.target.files[0]);
         props.setResumeFile(e.target.files[0]);
     };
 
@@ -34,7 +44,11 @@ const PdfUpload = (props: any) => {
           <br/>
           {props.resumeFile &&
           <span style={{'fontSize': '14px'}}>
-            Filename: {props.resumeFile.name} <FontAwesomeIcon icon={faCheck} style={{color: "#34b233", paddingLeft: '10px'}} />
+            Filename: {props.resumeFile.name} {
+                isValid ? <FontAwesomeIcon icon={faCheck} style={{color: "#34b233", paddingLeft: '10px'}} />
+                :
+                <FontAwesomeIcon icon={faXmark} style={{color: "#FF0033", paddingLeft: '10px'}} />
+            }
           </span>}
           </div>
         </div>
